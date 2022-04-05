ae35ea8a115932e9b0404ce7f1cdc7a430eccced
Pelayo PAREJO PAGADOR <pelayo.parejopagador@gmail.com>
2022-04-05 06:15:06 +0200

    Added command line arguemnts

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,31 @@ module Main where
 
 import Data.Text (Text)
 import Reader ( readGrid )
-import Sudoku ( gridToText, gridToTextChoices, runReduceGridChoices, solve)
+import Sudoku ( gridToText, solve)
+import System.Exit
+import System.Environment
 
-readLines :: FilePath -> IO [String]
-readLines = fmap lines . readFile
+
 
 main :: IO ()
-main = do
-  lines <- readLines "grids/hard.txt"
+main = getArgs >>= parse >>= runMain
+
+runMain :: [String] -> IO ()
+runMain lines = do
   let grid = readGrid lines
   case grid of
     Nothing -> putStrLn "Invalid board"
     Just g -> case solve g of
       Nothing -> putStrLn "Impossible board"
-      Just g' -> putStrLn $ gridToTextChoices g'
+      Just g' -> putStrLn $ gridToText g'
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
+
+parse ["-h"] = usage >> exit
+parse [] = usage >> exit
+parse file = concat `fmap` mapM readLines file
+
+usage = putStrLn "Usage: stack exec -- sudoku-solver-exe [-h] [file]"
+exit = exitWith ExitSuccess
+die = exitWith (ExitFailure 1)
